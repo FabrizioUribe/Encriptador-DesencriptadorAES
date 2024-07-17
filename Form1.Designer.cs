1bb1620e44c5d228b95401efe9fd8168d8cc4c94
fabrizio uribe <fabrizio.uribe@gmail.com>
2024-07-16 23:18:31 -0500

    Proyecto Subido

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace GeneradorClaves_V1._0._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Cadena = new System.Windows.Forms.Label();
+            this.txtCadena = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbOpciones = new System.Windows.Forms.ComboBox();
+            this.btnEncriptar = new System.Windows.Forms.Button();
+            this.btnDesencriptar = new System.Windows.Forms.Button();
+            this.txtMostrar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Cadena
+            // 
+            this.Cadena.BackColor = System.Drawing.Color.Transparent;
+            this.Cadena.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cadena.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Cadena.Location = new System.Drawing.Point(12, 22);
+            this.Cadena.Name = "Cadena";
+            this.Cadena.Size = new System.Drawing.Size(75, 20);
+            this.Cadena.TabIndex = 0;
+            this.Cadena.Text = "Cadena";
+            this.Cadena.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtCadena
+            // 
+            this.txtCadena.Location = new System.Drawing.Point(95, 22);
+            this.txtCadena.Name = "txtCadena";
+            this.txtCadena.Size = new System.Drawing.Size(276, 20);
+            this.txtCadena.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Opciones";
+            // 
+            // cmbOpciones
+            // 
+            this.cmbOpciones.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cmbOpciones.FormattingEnabled = true;
+            this.cmbOpciones.Location = new System.Drawing.Point(94, 54);
+            this.cmbOpciones.Name = "cmbOpciones";
+            this.cmbOpciones.Size = new System.Drawing.Size(277, 21);
+            this.cmbOpciones.TabIndex = 3;
+            // 
+            // btnEncriptar
+            // 
+            this.btnEncriptar.BackColor = System.Drawing.Color.Crimson;
+            this.btnEncriptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncriptar.ForeColor = System.Drawing.Color.White;
+            this.btnEncriptar.Location = new System.Drawing.Point(15, 90);
+            this.btnEncriptar.Name = "btnEncriptar";
+            this.btnEncriptar.Size = new System.Drawing.Size(180, 42);
+            this.btnEncriptar.TabIndex = 4;
+            this.btnEncriptar.Text = "Encriptar Cadena";
+            this.btnEncriptar.UseVisualStyleBackColor = false;
+            this.btnEncriptar.Click += new System.EventHandler(this.btnEncriptar_Click);
+            // 
+            // btnDesencriptar
+            // 
+            this.btnDesencriptar.BackColor = System.Drawing.Color.Crimson;
+            this.btnDesencriptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesencriptar.ForeColor = System.Drawing.Color.White;
+            this.btnDesencriptar.Location = new System.Drawing.Point(201, 90);
+            this.btnDesencriptar.Name = "btnDesencriptar";
+            this.btnDesencriptar.Size = new System.Drawing.Size(170, 42);
+            this.btnDesencriptar.TabIndex = 5;
+            this.btnDesencriptar.Text = "Desencriptar Cadena";
+            this.btnDesencriptar.UseVisualStyleBackColor = false;
+            this.btnDesencriptar.Click += new System.EventHandler(this.btnDesencriptar_Click);
+            // 
+            // txtMostrar
+            // 
+            this.txtMostrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtMostrar.Location = new System.Drawing.Point(15, 138);
+            this.txtMostrar.Multiline = true;
+            this.txtMostrar.Name = "txtMostrar";
+            this.txtMostrar.Size = new System.Drawing.Size(356, 224);
+            this.txtMostrar.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(389, 374);
+            this.Controls.Add(this.txtMostrar);
+            this.Controls.Add(this.btnDesencriptar);
+            this.Controls.Add(this.btnEncriptar);
+            this.Controls.Add(this.cmbOpciones);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCadena);
+            this.Controls.Add(this.Cadena);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Generador de Claves";
+            this.Load += new System.EventHandler(this.FormLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Cadena;
+        private System.Windows.Forms.TextBox txtCadena;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbOpciones;
+        private System.Windows.Forms.Button btnEncriptar;
+        private System.Windows.Forms.Button btnDesencriptar;
+        private System.Windows.Forms.TextBox txtMostrar;
+    }
+}
+
---
+++ GeneradorClaves_V1.0.3/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace GeneradorClaves_V1._0._3
+{
+    public partial class Form1 : Form
+    {
+        public static bool bandera = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public string ValidarOpcionesEncriptar()
+        {
+
+            int cm = cmbOpciones.SelectedIndex + 1;
+            string result = "", result2 = "";
+            switch (cm)
+            {
+                case 1:
+                    result = EncriptarUsuarios(txtCadena.Text);
+                    result2 = EncriptarUsuarios(result);
+                    break;
+                case 2: result2 = CifrarTextoAES(txtCadena.Text, "SHA1", 256, 0); break;
+                case 3: result2 = CifrarTextoAES(txtCadena.Text, "SHA1", 256, 50); break;
+            }
+            return result2;
+        }
+        public string ValidarOpcionesDesencriptar()
+        {
+            int cm = cmbOpciones.SelectedIndex + 1;
+            string result = "", result2 = "";
+
+            string data = txtCadena.Text.ToString();
+            switch (cm)
+            {
+                case 1:
+                    result = DesEncriptarUsuarios(txtCadena.Text);
+                    result2 = DesEncriptarUsuarios(result);
+                    break;
+                case 2: result2 = DescifrarTextoAES(txtCadena.Text, "SHA1", 256, 0); break;
+                case 3: result2 = DescifrarTextoAES(txtCadena.Text, "SHA1", 256, 50); break;
+            }
+            return result2;
+        }
+        private void btnEncriptar_Click(object sender, EventArgs e)
+        {
+            ValidarCadena();
+            if (!bandera)
+            {
+                txtMostrar.Text = ValidarOpcionesEncriptar();
+                MessageBox.Show("Se ha encriptado la cadena de conexion");
+            }
+        }
+
+        private void btnDesencriptar_Click(object sender, EventArgs e)
+        {
+            ValidarCadena();
+
+            if (!bandera)
+            {
+                //txtMostrar.Text = DescifrarTextoAES(txtCadena.Text, "SHA1", 256, 50);
+                txtMostrar.Text = ValidarOpcionesDesencriptar();
+                MessageBox.Show("Se ha desencriptado la cadena de conexion");
+            }
+        }
+        public bool ValidarCadena()
+        {
+            if (txtCadena.Text == null || txtCadena.Text == "")
+            {
+                MessageBox.Show("Debe Ingresar una Cadena de texto");
+                bandera = true;
+            }
+            else
+            {
+                bandera = false;
+            }
+            return bandera;
+        }
+        private static string EncriptarUsuarios(string _cadenaAencriptar)
+        {
+            try
+            {
+                if (_cadenaAencriptar == "")
+                {
+                    return "";
+                }
+
+                string result = string.Empty;
+                byte[] encryted = System.Text.Encoding.Unicode.GetBytes(_cadenaAencriptar);
+                result = Convert.ToBase64String(encryted);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+        private static string DesEncriptarUsuarios(string _cadenaAdesencriptar)
+        {
+            try
+            {
+                string result = string.Empty;
+                byte[] decryted = Convert.FromBase64String(_cadenaAdesencriptar);
+                //result = System.Text.Encoding.Unicode.GetString(decryted, 0, decryted.ToArray().Length);
+                result = System.Text.Encoding.Unicode.GetString(decryted);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+
+        }
+        private string CifrarTextoAES(string textoCifrar, string algoritmoEncriptacionHASH, int tamanoClave, int desc)
+        {
+            try
+            {
+                string palabraPaso = "c13l03l3naf32r3l44ndr34l3n1nr0b1ns0n";
+                string valorRGBSalt = desc == 0 ? "r4m1r3z1ng11ng44g21l4rr4m1r3zc0ll4z0s" : "r4m1r3z1ng11ng44" + desc + "21l4rr4m1r3zc0ll4z0s";
+                string vectorInicial = "32st4q214j4c1nt0";
+
+                int iteraciones = 50;
+                byte[] InitialVectorBytes = Encoding.ASCII.GetBytes(vectorInicial);
+                byte[] saltValueBytes = Encoding.ASCII.GetBytes(valorRGBSalt);
+                byte[] plainTextBytes = Encoding.UTF8.GetBytes(textoCifrar);
+
+                PasswordDeriveBytes password = new PasswordDeriveBytes(palabraPaso, saltValueBytes, algoritmoEncriptacionHASH, iteraciones);
+
+                byte[] keyBytes = password.GetBytes(tamanoClave / 8);
+                RijndaelManaged symmetricKey = new RijndaelManaged();
+
+
+                symmetricKey.Mode = CipherMode.CBC;
+                ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, InitialVectorBytes);
+
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+
+                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                cryptoStream.FlushFinalBlock();
+                byte[] cipherTextBytes = memoryStream.ToArray();
+                memoryStream.Close();
+                cryptoStream.Close();
+                string textoCifradoFinal = Convert.ToBase64String(cipherTextBytes);
+                return textoCifradoFinal;
+
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+        private string DescifrarTextoAES(string textoCifrado, string algoritmoEncriptacionHASH, int tamanoClave, int desc)
+        {
+            try
+            {
+                string palabraPaso = "c13l03l3naf32r3l44ndr34l3n1nr0b1ns0n";
+                string valorRGBSalt = "r4m1r3z1ng11ng44" + desc + "21l4rr4m1r3zc0ll4z0s";
+                string vectorInicial = "32st4q214j4c1nt0";
+                int iteraciones = 50;
+                byte[] InitialVectorBytes = Encoding.ASCII.GetBytes(vectorInicial);
+                byte[] saltValueBytes = Encoding.ASCII.GetBytes(valorRGBSalt);
+
+                byte[] cipherTextBytes = Convert.FromBase64String(textoCifrado);
+
+                PasswordDeriveBytes password =
+                    new PasswordDeriveBytes(palabraPaso, saltValueBytes,
+                        algoritmoEncriptacionHASH, iteraciones);
+
+                byte[] keyBytes = password.GetBytes(tamanoClave / 8);
+
+                RijndaelManaged symmetricKey = new RijndaelManaged();
+
+                symmetricKey.Mode = CipherMode.CBC;
+
+                if ((InitialVectorBytes.Length == 0))
+                    symmetricKey.Mode = CipherMode.ECB;
+                else
+                    symmetricKey.Mode = CipherMode.CBC;
+
+
+                ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, InitialVectorBytes);
+
+                MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+
+                byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+                int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+
+                memoryStream.Close();
+                cryptoStream.Close();
+
+                string textoDescifradoFinal = Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+
+                return textoDescifradoFinal;
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+        public class ListarOpciones
+        {
+            public int Id { get; set; }
+            public string valor { get; set; }
+
+            public ListarOpciones(int _id, string _valor)
+            {
+                Id = _id;
+                valor = _valor;
+            }
+            public override string ToString()
+            {
+                return valor;
+            }
+        }
+
+        private void FormLoad(object sender, EventArgs e)
+        {
+            txtMostrar.ReadOnly = true;
+            cmbOpciones.Items.Add(new ListarOpciones(1, "Claves de usuario"));
+            cmbOpciones.Items.Add(new ListarOpciones(2, "Licencia de dispositivos"));
+            cmbOpciones.Items.Add(new ListarOpciones(3, "Cadena de Conexión"));
+            cmbOpciones.SelectedIndex = 0;
+            //this.Sonido();
+        }
+        private void Sonido()
+        {
+            SoundPlayer sonido = new SoundPlayer();
+            sonido.SoundLocation = "D:/fondo.wav";
+            sonido.Play();
+        }
+    }
+}
